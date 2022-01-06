d5d65c0c9b48775328d03429d9125dead8531c07
Rene Roscher <roscher794@gmail.com>
2022-01-06 03:33:56 +0100

    add: file helper methods

--- app/helpers.php
+++ app/helpers.php
@@ -1,1 +1,31 @@
 <?php
+
+function user()
+{
+    return auth()->user();
+}
+
+function translations($locale)
+{
+    $path = resource_path("lang/$locale.json");
+    if (!file_exists($path)) {
+        return [];
+    }
+    return json_decode(file_get_contents($path), true);
+}
+
+function flash($message, $error = false)
+{
+    return back()->with('flash', [
+        'bannerStyle' => $error ? 'danger' : 'success',
+        'banner' => $message,
+    ]);
+}
+
+function flashRedirect($name, $message, $error = false)
+{
+    return redirect($name)->with('flash', [
+        'bannerStyle' => $error ? 'danger' : 'success',
+        'banner' => $message,
+    ]);
+}
