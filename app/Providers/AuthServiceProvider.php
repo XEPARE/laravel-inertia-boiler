eba370be5774f852508e64fe52333e991de82563
Rene Roscher <roscher794@gmail.com>
2022-01-06 06:43:42 +0100

    add: bypass to SuperAdmin role

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\Role;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -25,6 +26,8 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        \Gate::before(function ($user, $ability) {
+            return $user->hasRole(Role::SUPER_ADMIN) ? true : null;
+        });
     }
 }
