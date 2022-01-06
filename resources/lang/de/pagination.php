024a669fad3454a2d9b820ca173646e09975ad3c
Rene Roscher <roscher794@gmail.com>
2022-01-06 06:14:49 +0100

    add: language DE

--- resources/lang/de/pagination.php
+++ resources/lang/de/pagination.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Pagination Language Lines
@@ -13,7 +12,6 @@ return [
     |
     */
 
-    'previous' => 'Previous',
-    'next' => 'Next',
-
+    'previous' => 'Zurück',
+    'next'     => 'Weiter',
 ];
