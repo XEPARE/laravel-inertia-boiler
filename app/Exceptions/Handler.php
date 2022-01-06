2324b899de4a37ff7f568668e0ec4bae045db732
Rene Roscher <roscher794@gmail.com>
2022-01-06 06:56:09 +0100

    add: sentry catch

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -38,4 +38,14 @@ class Handler extends ExceptionHandler
             //
         });
     }
+
+    public function report(Throwable $exception)
+    {
+        if (app()->bound('sentry') && $this->shouldReport($exception)) {
+            app('sentry')->captureException($exception);
+        }
+
+        parent::report($exception);
+    }
+
 }
