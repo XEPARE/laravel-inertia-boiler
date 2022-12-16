0623ce3e0ed7219414c479756c7aa947f6fbdc31
Rene Roscher <roscher794@gmail.com>
2022-12-16 21:29:16 +0100

    added enum to usere

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Enums\StatusEnum;
 use App\Helpers\Traits\CacheableTrait;
 use App\Helpers\Traits\SearchableScope;
 use App\Helpers\Traits\SearchableTrait;
@@ -70,6 +71,7 @@ class User extends Authenticatable /*implements MustVerifyEmail*/
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'state' => StatusEnum::class,
     ];
 
     /**
