100558f395e180576135bcf5df34a06654d68ba5
Rene Roscher <roscher794@gmail.com>
2022-12-16 19:20:05 +0100

    generated imports in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Model;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        \App\Models\Model::preventLazyLoading();
-        \App::setLocale('de');
+        Model::preventLazyLoading();
+        App::setLocale('de');
     }
 }
