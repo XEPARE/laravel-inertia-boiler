a9e90b32cf976f9a0b62632828cf6c1c8f45cf96
Rene Roscher <roscher794@gmail.com>
2023-02-12 00:36:35 +0100

    improved Model (replaced HasSnowflake with HasShortflakePrimary / js can't handling 64bit ids / now 53bit ids)

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -3,26 +3,18 @@
 
 namespace App\Models;
 
-
 use App\Helpers\Traits\SearchableScope;
 use App\Helpers\Traits\SearchableTrait;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\SoftDeletes;
-use Kra8\Snowflake\HasSnowflakePrimary;
+use Kra8\Snowflake\HasShortflakePrimary;
 
 /**
  * @mixin IdeHelperModel
  */
 class Model extends \Illuminate\Database\Eloquent\Model
 {
-    use HasFactory, HasSnowflakePrimary, SearchableTrait, SearchableScope;
+    use HasShortflakePrimary, SearchableTrait, SearchableScope;
 
     protected $keyType = 'string';
     public $incrementing = false;
-
-    public function getIdAttribute()
-    {
-        return (string) $this->id;
-    }
 
 }
