9cf7b2c856b1dfde93fd53a9a83538393292dd97
Rene-Roscher <43115138+Rene-Roscher@users.noreply.github.com>
2023-06-06 15:15:36 +0200

    Update HasMedia.php

--- app/Traits/HasMedia.php
+++ app/Traits/HasMedia.php
@@ -4,18 +4,18 @@ namespace App\Traits;
 
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Str;
 
 trait HasMedia
 {
     /**
      * Update the user's media file.
      *
-     * @param  \Illuminate\Http\UploadedFile  $media
-     * @param  string  $path
+     * @param \Illuminate\Http\UploadedFile $media
+     * @param string $pathColumn
+     * @param string $path
      * @return void
      */
-    public function updateMedia(UploadedFile $media, $pathColumn = 'profile_photo_path', $path = 'profile-photos')
+    public function updateMedia(UploadedFile $media, string $pathColumn = 'profile_photo_path', string $path = 'profile-photos'): void
     {
         tap($this->{$pathColumn}, function ($previous) use ($media, $path) {
             $this->forceFill([
@@ -36,7 +36,7 @@ trait HasMedia
      * @param  string  $path
      * @return void
      */
-    public function deleteMedia($path)
+    public function deleteMedia($path): void
     {
         if (is_null($this->{$path})) {
             return;
@@ -56,7 +56,7 @@ trait HasMedia
      * @param  string  $defaultUrl
      * @return string
      */
-    public function getMediaUrl($path, $defaultUrl)
+    public function getMediaUrl($path, $defaultUrl): string
     {
         return $this->{$path}
                     ? Storage::disk($this->mediaDisk())->url($this->{$path})
@@ -68,7 +68,7 @@ trait HasMedia
      *
      * @return string
      */
-    protected function mediaDisk()
+    protected function mediaDisk(): string
     {
         return isset($_ENV['VAPOR_ARTIFACT_NAME']) ? 's3' : config('filesystems.default');
     }
